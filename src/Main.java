7261ac60de5559411f83c7aa6be63d9e4343dbbf
Alexander Lukyanov <lux-nova@mail.ru>
2022-12-16 14:47:27 +0300

    interactive regime added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -12,80 +13,97 @@ public class Main {
             *    1 = интерактивный режим                                 *
             *    . = завершение                                          *
             * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *""";
+    static String PROMPT = """
+                   Интерактивная работа с Колодой целочисленных значений.
+                   Используйте следующие команды:
+                       "положить х"  = добавить число х сверху колоды
+                       "взять"       = снять и показать число, лежавшее наверху колоды
+                       "перевернуть" = обратить порядок чисел в колоде
+                       "показать"    = отразить текущее состояние колоды
+                       "опустошить"  = обнулить содержимое колоды
+                       "хватит"      = завершение интерактивного режима
+                    
+                """;
     public static void main(String[] args) {
-        System.out.println(WELCOME);
-        String input = keyboard.next();
         while (true) {
+            System.out.println(WELCOME);
+            String input = keyboard.next();
             if ("0".equals(input)) demo();
             if ("1".equals(input)) work();
             if (".".equals(input)) break;
-            System.out.println(WELCOME);
-            input = keyboard.next();
         }
         System.out.println("Работа программы демонстрации завершена.");
     }
 
     private static void work() {
+        System.out.println(PROMPT);
+        boolean exit = false;
+        String command;
+        while (!exit) {
+            command = keyboard.next();
+
+            if (command.startsWith("положить ")) {
+                int enteredValue;
+                try {
+                    enteredValue = Integer.parseInt(
+                            command.substring(command.indexOf(" ") + 1));
+                    стог.положить(enteredValue);
+                } catch (NumberFormatException t) {
+                    System.out.println("не удалось распознать число");
+                }
+                continue;
+            }
+
+            switch (command) {
+                case "взять" -> System.out.println("взято с колоды: " + стог.взять());
+                case "перевернуть" -> стог = стог.перевернуть();
+                case "показать" -> стог.напечатать();
+                case "опустошить" -> стог = new Колода<>();
+                case "хватит" -> exit = true;
+                default -> System.out.println(PROMPT);
+
+            }
+        }
+        System.out.println("Выход из интерактивнго режима.");
     }
 
     private static void demo() {
         System.out.println("Последовательность команд из задания https://github.com/netology-code/algo-homeworks/blob/master/tasks/Lists.md\n");
+
         стог.напечатать();
-        
-        System.out.println("Добавим 0");
-        стог.положить(0);
+
+        IntStream.range(0, 6).forEach(Main::put_and_print);
+
+        take_and_print();
+        take_and_print();
+
+        reverse_and_print();
+
+        take_and_print();
+        take_and_print();
+
+        reverse_and_print();
+
+        take_and_print();
+        take_and_print();
+    }
+
+    private static void put_and_print(int put) {
+        System.out.println("Добавим " + put);
+        стог.положить(put);
         стог.напечатать();
-        
-        System.out.println("Добавим 1");
-        стог.положить(1);
-        стог.напечатать();
-        
-        System.out.println("Добавим 2");
-        стог.положить(2);
-        стог.напечатать();
-        
-        System.out.println("Добавим 3");
-        стог.положить(3);
-        стог.напечатать();
-        
-        System.out.println("Добавим 4");
-        стог.положить(4);
-        стог.напечатать();
-        
-        System.out.println("Добавим 5");
-        стог.положить(5);
-        стог.напечатать();
-        
-        System.out.println("Снимем со стека");
-        System.out.println(стог.взять());
-        стог.напечатать();
-        
-        System.out.println("Снимем со стека");
-        System.out.println(стог.взять());
-        стог.напечатать();
-        
-        System.out.println("Ревёрс!");
-        стог = стог.перевернуть();
-        стог.напечатать();
-        
-        System.out.println("Снимем со стека");
-        System.out.println(стог.взять());
-        стог.напечатать();
-        
-        System.out.println("Снимем со стека");
-        System.out.println(стог.взять());
-        стог.напечатать();
-        
-        System.out.println("Ревёрс!");
-        стог = стог.перевернуть();
-        стог.напечатать();
-        
-        System.out.println("Снимем со стека");
-        System.out.println(стог.взять());
-        стог.напечатать();
-        
+    }
+
+    private static void take_and_print() {
         System.out.println("Снимем со стека");
         System.out.println(стог.взять());
         стог.напечатать();
     }
+
+    private static void reverse_and_print() {
+        System.out.println("Ревёрс!");
+        стог = стог.перевернуть();
+        стог.напечатать();
+    }
+
 }
--- src/Колода.java
+++ src/Колода.java
@@ -1,23 +1,61 @@
+/**
+ * This pretty implementation of stack for everyday usage is designed
+ * for people's convenience, peace on the Earth and the Russian World's propagation.
+ * It defines methods for pushing, popping, reversing and printing the stuff.
+ * @param <T>   the type of data that the stack is meant to store and operate with.
+ */
 public class Колода<T> {
 
+    /**
+     * The node lying on the top of the pack.
+     */
     private Элемент<T> верх;
 
+    /**
+     * The node modelling the items being stored.
+     * @param <T> the type of srored data.
+     */
     protected class Элемент<T> {
+
+        /**
+         * The matter to be stored.
+         */
         T значение;
+
+        /**
+         * The previous item been put in the pack,
+         * thus lying under the given one.
+         */
         Элемент<T> следующий;
 
+        /**
+         * A new item containing the value and lying over the another.
+         * @param значение  a value this item holds.
+         * @param следующий another item this item lies over.
+         */
         public Элемент(T значение, Элемент<T> следующий) {
             this.значение = значение;
             this.следующий = следующий;
         }
     }
 
+    /**
+     * Puts a new value on the top of the stack.
+     * A former top value becomes 'previous (next in the pack)' for the new been put.
+     * @param значение the value being put on the pack.
+     */
     public void положить(T значение) {
 
         верх = new Элемент<>(значение, верх);
 
     }
 
+    /**
+     * Takes off and returns the value from the top of the stack.
+     * The length of the stack gets reduced by one (if not empty already),
+     * and the previous (next in the pack) item having been lying below turns to be the top one.
+     * @return the value situated on the top of the pack, or {@code null} if the stack is empty.
+     */
     public T взять() {
 
         if (верх == null)
@@ -30,6 +68,10 @@ public class Колода<T> {
         return отдаваемое;
     }
 
+    /**
+     * Returns a new Колода containing all items of the stack in reversed order.
+     * @return the inversed copy of the stack, or an empty stack if the original one is empty.
+     */
     public Колода<T> перевернуть() {
         Колода<T> перевёрнутая = new Колода<>();
 //        Колода<T> копия = (Колода<T>) this.clone();
@@ -41,6 +83,11 @@ public class Колода<T> {
         return перевёрнутая;
     }
 
+    /**
+     * Prints out an arrowed chain-like representation of the stack's content,
+     * bordered by curly braces for better distinguishably.
+     * If there's no items inside, reports it explicitly.
+     */
     public void напечатать() {
         StringBuilder sb = new StringBuilder();
         if (верх != null) {
